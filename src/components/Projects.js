b921cad847bf9bf85d3761dccaadfeed659167bf
Oreno26 <orendanger@hotmail.com>
2023-05-03 16:31:30 +0300

    skills and font

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,21 +9,21 @@ import Mem from "./img/memory.jpg"
 
 const Projects = (props) => {
   return (
-    <Box  id="projects" sx={{m: 2}} style={{ width: "60vw", height:'100vh' }} >
+    <Box  id="projects" sx={{m: 2}} style={{ width: "60vw", height:'100vh', }} >
       <h1>Projects</h1>
       <Carousel >
         <div >
           <img src={WTR}  />
-          <p className="legend"><a href="https://wtr.onrender.com">Weather The Ride</a><br/>  
-          <a href="https://github.com/oreno26/WTR">Github</a></p>
+          <p className="legend"><a href="https://wtr.onrender.com" target="blank">Weather The Ride</a><br/>  
+          <a href="https://github.com/oreno26/WTR" target="blank">Github</a></p>
         </div>
         <div>
           <img src={BUDGET} />
-          <p className="legend"><a href="https://github.com/oreno26/project_budget">Github</a></p>
+          <p className="legend"><a href="https://github.com/oreno26/project_budget" target="blank">Github</a></p>
         </div>
         <div>
           <img src={Mem} />
-          <p className="legend"><a href="https://github.com/oreno26/Memory_game">Github</a></p>
+          <p className="legend"><a href="https://github.com/oreno26/Memory_game" target="blank">Github</a></p>
         </div>
       </Carousel>
     </Box>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,30 @@
-const Skills = (props) =>{
-    return(
-        <h1 id="skills">Skills</h1>
-    )
-}
-export default Skills
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core";
+import { SizeProp } from "@fortawesome/fontawesome-svg-core";
+import "./Skills.css"
+import {
+  faHtml5,
+  faNodeJs,
+  faNode,
+  faCss3,
+  faJs,
+  faReact,
+} from "@fortawesome/free-brands-svg-icons";
+
+
+const Skills = (props) => {
+  return (
+    <>
+    <div style={{paddingTop: '10px'}} id="skills"></div>
+      <h1>Skills</h1>
+      <div style={{display:'flex', justifyContent: 'space-evenly', width:'30vw'}}>
+        <FontAwesomeIcon className="skill" icon={faHtml5} />
+        <FontAwesomeIcon className="skill" icon={faNode}  />
+        <FontAwesomeIcon className="skill" icon={faCss3}  />
+        <FontAwesomeIcon className="skill" icon={faJs} />
+        <FontAwesomeIcon className="skill" icon={faReact}  />
+      </div>
+    </>
+  );
+};
+export default Skills;
